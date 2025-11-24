876c2427960d431782a0f6ca3e41640bd101097c
filippovm <filippovm@msoe.edu>
2025-11-24 10:47:13 -0600

    reorg files

---
+++ costco/src/main/java/com/databases/hm/costco/CostcoApplication.java
@@ -0,0 +1,13 @@
+package com.databases.hm.costco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostcoApplication.class, args);
+	}
+
+}
